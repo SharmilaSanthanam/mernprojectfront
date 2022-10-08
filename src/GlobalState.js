4a0da2bb3df4edb2bfa1f259183b1e886fd077f6
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2022-10-08 15:37:13 +0530

    Update GlobalState.js

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -18,7 +18,7 @@ export const DataProvider = ({children}) =>{
             const refreshToken = async () =>{
                 const res = await axios.get('https://craft123.herokuapp.com/user/refresh_token')
 //                    const res = await AxiosInstance.get('/user/refresh_token')
-        
+        console.log(res.data.accesstoken)
                 setToken(res.data.accesstoken)
     
                 setTimeout(() => {
