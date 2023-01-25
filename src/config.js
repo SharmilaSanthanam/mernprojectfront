af6b6f97e67e45493b98290a1b956b48b6107ed9
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2023-01-25 15:56:10 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-    baseURL: "https://craft123.herokuapp.com/"
+//     baseURL: "https://craft123.herokuapp.com/"
+    baseURL: "https://craftproject.onrender.com"
+  
 })
 
 export default AxiosInstance;
